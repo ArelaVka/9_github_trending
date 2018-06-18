64ba042a41c83854d9af5e140ee4fcc80c12c8d1
maltsev <arelav90@mail.ru>
2018-06-18 17:43:01 +0300

    get json from github

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,26 @@
+import requests
+import datetime as dt
 
 
 def get_trending_repositories(top_size):
-    pass
+    search_date = str(dt.date.today() - dt.timedelta(days=1))
+    search_params = {
+        'q': 'created: >' + search_date,
+        'sort': 'star',
+        'order': 'desc'
+    }
+    repos = requests.get(
+        'https://api.github.com/search/repositories',
+        params=search_params
+    ).json()
+    return repos
 
 
 def get_open_issues_amount(repo_owner, repo_name):
     pass
 
+
 if __name__ == '__main__':
-    pass
+    top_size = 20
+    print(get_trending_repositories(top_size))
+    print(str(dt.date.today() - dt.timedelta(weeks=1)))
