a850ca3fd27ea94473539929c23147d157fbb280
maltsev <arelav90@mail.ru>
2018-06-19 13:02:03 +0300

    add print issue

--- github_trending.py
+++ github_trending.py
@@ -4,29 +4,40 @@ import datetime as dt
 
 def get_trending_repositories(top_size):
     search_date = str(dt.date.today() - dt.timedelta(weeks=1))
-    print(search_date)
     search_params = {
-        'q': 'created>{}'.format(search_date),
+        'q': 'created:>' + search_date,
         'sort': 'stars'
     }
     top_repos = requests.get(
         'https://api.github.com/search/repositories',
         params=search_params
-    ).json()['items']
+    ).json()['items'][:top_size]
     return top_repos
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    url = 'https://api.github.com/repos/{}/{}/issues'.format(
-        repo_owner,
+def get_open_issues_amount(repo_name):
+    url = 'https://api.github.com/repos/{}/issues'.format(
         repo_name
     )
-    return requests.get(url).json
+    return requests.get(url).json()
 
 
 if __name__ == '__main__':
     top_size = 20
     top_repos = get_trending_repositories(top_size)
     for repo in top_repos:
-        print(repo['full_name'])
-
+        repo_owner = repo['owner']['login']
+        repo_name = repo['name']
+        repo_url = repo['html_url']
+        print(
+            '\nAuthor: {} \nRepo: {}'.format(
+                repo_owner,
+                repo_url)
+        )
+        issues = get_open_issues_amount(repo_name)
+        print('Issues url of repo:')
+        for issue in issues:
+            if issue:
+                print(issue['html_url'])
+            else:
+                print('Not found any issues')
