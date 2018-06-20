a2280046c33dac6bca75d2a33d8a5acf18fb575e
maltsev <arelav90@mail.ru>
2018-06-20 09:54:04 +0300

    add print func

--- github_trending.py
+++ github_trending.py
@@ -1,7 +1,6 @@
 import requests
 import datetime as dt
 import sys
-import json
 
 
 def get_trending_repositories(top_size):
@@ -17,36 +16,39 @@ def get_trending_repositories(top_size):
     return top_repos
 
 
-def get_open_issues_amount(repo_owner, repo_name):
+def get_open_issues_amount(repo):
+    repo_owner = repo['owner']['login']
+    repo_name = repo['name']
     url = 'https://api.github.com/repos/{}/{}/issues'.format(
         repo_owner,
         repo_name
     )
     open_issues = requests.get(url).json()
-    if open_issues == []:
-        return False
-    else:
+    if not(open_issues == []):
         return open_issues
+
+
+def print_repos_info(repo):
+    repo_owner = repo['owner']['login']
+    repo_url = repo['html_url']
+    print('\n--Author: {}\n--Repo: {}'.format(repo_owner, repo_url))
+
+
+def print_repos_issue(issues):
+    if issues:
+        print('-Issues url of repo:')
+        for issue in issues:
+            if issue == 'message':
+                sys.exit('Aborted: You have API rate limit exceeded!')
+            print('issue-html:', issue['html_url'])
+    else:
+        print('No issue was found!')
 
 
 if __name__ == '__main__':
     top_size = 20
     top_repos = get_trending_repositories(top_size)
     for repo in top_repos:
-        repo_owner = repo['owner']['login']
-        repo_name = repo['name']
-        repo_url = repo['html_url']
-        print(
-            '\n--Author: {}\n--Repo: {}'.format(
-                repo_owner,
-                repo_url)
-        )
-        issues = get_open_issues_amount(repo_owner, repo_name)
-        if issues:
-            print('-Issues url of repo:')
-            for issue in issues:
-                if issue == 'message':
-                    sys.exit('Aborted: You have API rate limit exceeded!')
-                print('issue-html:', issue['html_url'])
-        else:
-            print('No issue was found!')
+        print_repos_info(repo)
+        issues = get_open_issues_amount(repo)
+        print_repos_issue(issues)
